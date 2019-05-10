51cf190c0a71cdd83ff5d66b2ed983019520f2d0
nazmariam <fedirko.mary@gmail.com>
2019-05-10 17:45:57 +0300

    prettier

---
+++ build/precache-manifest.1c41046af3173c6818c4c586cbc6489f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa3e3e782f63125d8284686cb781962a",
+    "url": "/github-app/static/media/space2.fa3e3e78.jpg"
+  },
+  {
+    "revision": "7ede48462749a9986786",
+    "url": "/github-app/static/js/runtime~main.b924a25b.js"
+  },
+  {
+    "revision": "b87cfbc2d41b9e74158b",
+    "url": "/github-app/static/js/main.912c163d.chunk.js"
+  },
+  {
+    "revision": "6f1517b0a0a2ccdff7df",
+    "url": "/github-app/static/js/2.f22992bf.chunk.js"
+  },
+  {
+    "revision": "b87cfbc2d41b9e74158b",
+    "url": "/github-app/static/css/main.04fa853f.chunk.css"
+  },
+  {
+    "revision": "71197448914aaf21ab5320d130beaa9f",
+    "url": "/github-app/index.html"
+  }
+];
