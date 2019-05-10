66ce2f3b600b5ff167cebf160ef00049f086d606
nazmariam <fedirko.mary@gmail.com>
2019-05-10 17:21:13 +0300

    Updates

---
+++ precache-manifest.df20c1eb74dd0ff41ea28927958bcc69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa3e3e782f63125d8284686cb781962a",
+    "url": "/github-app/static/media/space2.fa3e3e78.jpg"
+  },
+  {
+    "revision": "7ede48462749a9986786",
+    "url": "/github-app/static/js/runtime~main.b924a25b.js"
+  },
+  {
+    "revision": "ea6722abe15ef809f1aa",
+    "url": "/github-app/static/js/main.db1b198a.chunk.js"
+  },
+  {
+    "revision": "6f1517b0a0a2ccdff7df",
+    "url": "/github-app/static/js/2.f22992bf.chunk.js"
+  },
+  {
+    "revision": "ea6722abe15ef809f1aa",
+    "url": "/github-app/static/css/main.a0b00bfb.chunk.css"
+  },
+  {
+    "revision": "82c26465be589ade7610e3697eba11b4",
+    "url": "/github-app/index.html"
+  }
+];
